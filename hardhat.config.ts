8dd1adc1be728f30734d3e5d5817fc349b0bd90b
JohnWon <1498118157@qq.com>
2026-04-19 18:21:17 +0800

    chore: change to the hardhat-solidity extension and modify the way for loading the hardhat environment
    Signed-off-by: JohnWon <1498118157@qq.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,15 @@
 import type { HardhatUserConfig } from 'hardhat/config'
-import dotenv from 'dotenv'
+import { vars } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox-viem'
 import '@nomicfoundation/hardhat-verify'
 import '@fhevm/hardhat-plugin'
 import 'hardhat-gas-reporter'
 import 'solidity-coverage'
 
-dotenv.config({ path: ['.env.local', '.env'] })
-
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL!
-const TEST_PRIVATE_KEY = process.env.TEST_PRIVATE_KEY!
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY!
-const COIN_MARKET_KEY = process.env.COIN_MARKET_KEY!
+const SEPOLIA_RPC_URL = vars.get('SEPOLIA_RPC_URL')
+const TEST_PRIVATE_KEY = vars.get('TEST_PRIVATE_KEY')
+const ETHERSCAN_API_KEY = vars.get('ETHERSCAN_API_KEY')
+const COIN_MARKET_KEY = vars.get('COIN_MARKET_KEY', 'xx')
 
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
