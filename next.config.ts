1e15d01e8e92739e71d14c9121fcf3f613345bb3
JohnWon <1498118157@qq.com>
2026-05-07 19:22:26 +0800

    chore: update resolve alias
    Signed-off-by: JohnWon <1498118157@qq.com>

--- next.config.ts
+++ next.config.ts
@@ -16,12 +16,14 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ['pino-pretty', 'lokijs', 'encoding'],
   turbopack: {
     resolveAlias: {
-      fs: './module-resolvers/empty-module.js',
-      util: './module-resolvers/util-browser.js',
-      net: './module-resolvers/empty-module.js',
-      tls: './module-resolvers/empty-module.js',
-      child_process: './module-resolvers/empty-module.js',
-      worker_threads: './module-resolvers/empty-module.js',
+      'fs': './module-resolvers/empty-module.js',
+      'util': './module-resolvers/util-browser.js',
+      'net': './module-resolvers/empty-module.js',
+      'tls': './module-resolvers/empty-module.js',
+      'child_process': './module-resolvers/empty-module.js',
+      'worker_threads': './module-resolvers/empty-module.js',
+      '@': './src',
+      '-': './',
     },
   },
 }
